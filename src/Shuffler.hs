29f0399c8462acc7d8661ae545e7457cdee86ebe
ShuaKo <goastig_321@yahoo.com>
2016-12-23 03:14:54 +0800

    modified shuffler to fix import problems

--- src/Shuffler.hs
+++ src/Shuffler.hs
@@ -1,9 +1,10 @@
 module Shuffler where
 
 import Common
-import System.Random.Shuffle
+import System.Random
 shuffleDeck :: State -> IO State
 
 -- TODO: Implement a random shuffling algorithm
-shuffleDeck state = do newDeck <- shuffleM (deck state)
-    				return ( state { deck = newDeck})
+shuffleDeck state = do
+    newDeck <- shuffle (deck state)
+    return ( state { deck = newDeck})
